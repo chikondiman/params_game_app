4f0c0126a001ac7d9018d82e85b6c943333e08ce
chikondi <chikondiman@gmail.com>
2020-05-09 17:45:07 -0400

    init commit

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,2 +1,27 @@
 class Api::ParamsController < ApplicationController
+  # def game
+  #   if params[:name][0] == "a"
+  #     @message = "Hey, your name starts with the first letter of the alphabet!"
+  #   else
+  #     @message = "your name does not begin with A"
+  #   end
+  #   render "query_params.json.jb"
+  # end
+
+  # def guess
+  #   user_guess = params[:user_guess].to_i
+  #   answer = 40
+  #   if user_guess > answer
+  #     @message = "you guessed too high"
+  #   elsif user_guess < answer
+  #     @message = "you guessed too low"
+  #   else
+  #     @message = "you win"
+  #   end
+  #   render "guess.json.jb"
+  # end
+
+  def guess_pass
+    render "guess_pass.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/url_segment_parameter/" => "params#guess"
+    post "/game_user_pass/" => "params_pass#guess"
+  end
 end
